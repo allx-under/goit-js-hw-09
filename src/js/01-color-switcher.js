d74589bb3c3ea113f0490c1bb0c20ac8ff80cc1a
allx-under <95172339+allx-under@users.noreply.github.com>
2022-06-10 14:37:53 +0300

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+};
+let intervalId = null;
+
+refs.startBtn.addEventListener('click',onClickChangeBgColor)
+refs.stopBtn.addEventListener('click', onClickRemoveBgActions)
+
+refs.stopBtn.disabled = true;
+
+function onClickChangeBgColor() {
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function onClickRemoveBgActions() {
+    clearInterval(intervalId);
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
