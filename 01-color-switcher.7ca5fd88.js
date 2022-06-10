7303868d3387ba48dc860876ea0d86a188ba7b24
allx-under <95172339+allx-under@users.noreply.github.com>
2022-06-10 11:38:31 +0000

    Deploying to gh-pages from @ allx-under/goit-js-hw-09@d74589bb3c3ea113f0490c1bb0c20ac8ff80cc1a 🚀

---
+++ 01-color-switcher.7ca5fd88.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.stopBtn.disabled=!0,t.startBtn.disabled=!1})),t.stopBtn.disabled=!0;
+//# sourceMappingURL=01-color-switcher.7ca5fd88.js.map
